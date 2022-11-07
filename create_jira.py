b7fd68a255cff1370e490f1ca3e423f4a02d85b9
rjanakiraman <rjanakiraman@vmware.com>
2022-11-07 19:59:04 +0530

    add JIRA creation step

--- create_jira.py
+++ create_jira.py
@@ -9,6 +9,7 @@ def parse_event_context():
     issue_type = ""
     issue_body = event_json["issue"]["body"]
     issue_title = event_json["issue"]["title"]
+    issue_url =  event_json["issue"]["url"]
     for label in event_json["issue"]["labels"]:
         for issue_type_obj in issue_types:
             for k,v in issue_type_obj.items():
@@ -17,13 +18,13 @@ def parse_event_context():
     if issue_type == "":
         print("Making default type as task")
         issue_type = "Task"
-    return issue_body, issue_type, issue_title
+    return issue_body, issue_type, issue_title, issue_url
     #print("Please set EVENT_CONTEXT Variable with github issue event type...")
     #sys.exit(1)
     #print("Could not continue creation of NPT JIRA ticket due to {}...".format(e))
     #sys.exit(1)
 
-def run_create_issue(issue_body, issue_type, issue_title):
+def run_create_issue(issue_body, issue_type, issue_title, issue_url):
     """
     JIRA_SERVER environment variable will be feed from Github CI for creating issues
     JIRA_USER, JIRA_PWD will be kept as a secret for authentication
@@ -44,16 +45,17 @@ def run_create_issue(issue_body, issue_type, issue_title):
     Issue = {
         'project': {'key': 'NPT'},
         'summary': issue_title ,
-        'description': issue_body,
+        'description': "{} \n Github Issue: {}".formt(issue_body,issue_url),
         'issuetype': {'name': issue_type },
     }
     try:
         new_issue = jira.create_issue(fields=Issue)
-        print("New Jinew_issue created {}".format(new_issue))
+        print("New Jira_issue created {}".format(new_issue))
+
     except Exception as e:
         print("Could not create JIRA due to : {}", e)
         sys.exit(1)
 
 if __name__ == "__main__":
-    issue_body, issue_type, issue_title = parse_event_context()
-    run_create_issue(issue_body, issue_type, issue_title)
+    issue_body, issue_type, issue_title, issue_url = parse_event_context()
+    run_create_issue(issue_body, issue_type, issue_title, issue_url)
